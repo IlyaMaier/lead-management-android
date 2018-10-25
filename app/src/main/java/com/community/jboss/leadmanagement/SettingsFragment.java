d300007fb40fb50fc20a3af8b5b9e761db8918e5
IlyaMaier <34242059+IlyaMaier@users.noreply.github.com>
2018-10-25 19:19:08 +0200

    Revert "GCI 2018 Splash Screen"

--- app/src/main/java/com/community/jboss/leadmanagement/SettingsFragment.java
+++ app/src/main/java/com/community/jboss/leadmanagement/SettingsFragment.java
@@ -2,9 +2,7 @@ package com.community.jboss.leadmanagement;
 
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,10 +11,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.widget.Toast;
 
 import com.community.jboss.leadmanagement.main.MainActivity;
-import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -42,32 +38,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
             final EditTextPreference mPreference = (EditTextPreference) findPreference("server_location");
             final SwitchPreference mToggleMode = (SwitchPreference) findPreference("dark_theme");
-            final Preference mSignOut = findPreference("sign_out");
-            mToggleMode.setOnPreferenceChangeListener((preference, newValue) -> {
-                mActivity.startActivity(new Intent(mActivity, MainActivity.class));
-                mActivity.finish();
-                return true;
+            mToggleMode.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    mActivity.startActivity(new Intent(mActivity, MainActivity.class));
+                    mActivity.finish();
+                    return true;
+                }
             });
             mPreference.setSummary(currentServer);
             mPreference.setText(currentServer);
 
-            FirebaseAuth mAuth = FirebaseAuth.getInstance();
-            mSignOut.setOnPreferenceClickListener(preference -> {
-                if(mAuth.getCurrentUser() == null) {
-                    Toast.makeText(getContext(), "Already signed out", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
-                alertBuilder.setTitle("Sign Out");
-                alertBuilder.setMessage("Are you sure you want to sign out?");
-                alertBuilder.setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
-                alertBuilder.setPositiveButton("Yes", (dialogInterface, i) -> {
-                    mAuth.signOut();
-                    Toast.makeText(getContext(), "Signed out", Toast.LENGTH_SHORT).show();
-                });
-                alertBuilder.create().show();
-                return true;
-            });
+
         }
     }
 }
